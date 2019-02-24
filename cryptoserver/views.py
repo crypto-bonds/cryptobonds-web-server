ba2239a1b6e09f224aba7950185f3953fcecad40
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-23 23:11:40 -0600

    Fixed request post parameters bug

--- cryptoserver/views.py
+++ cryptoserver/views.py
@@ -5,5 +5,10 @@ def index(request):
 
 
 def signin(request):
-    print(request.method)
-    return render(request, 'cryptoserver/bank_main.html')
+    selection = request.POST['inlineFormCustomSelect']
+    if selection == '1':
+        return render(request, 'cryptoserver/bank_main.html')
+    elif selection == '2':
+        return render(request, 'cryptoserver/company_main.html')
+    else:
+        return render(request, 'cryptoserver/trader_main.html')
