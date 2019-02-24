9c049fcf990e4989867b63637d2ada100c2e4a6e
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-24 00:37:47 -0600

    Added withdraw url in urls

--- cryptoserver/urls.py
+++ cryptoserver/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signin/', views.signin, name='signin'),
+    path('withdraw/', views.signin, name='withdraw'),
 ]
