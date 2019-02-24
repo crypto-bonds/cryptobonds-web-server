8898863484457e98268cbf3245736def9c06f9b8
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-24 04:58:18 -0600

    Configured urls for bond request and issue

--- cryptoserver/urls.py
+++ cryptoserver/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signin/', views.signin, name='signin'),
+
     path('<int:company_id>/company_withdraw/', views.cwithdraw, name='cwithdraw'),
     path('<int:company_id>/company_deposit/', views.cdeposit, name='cdeposit'),
     path('<int:company_id>/company_request/', views.crequest, name='crequest'),
+    path('bond_request/', views.bond_request, name='bond_request'),
 
     path('<int:bank_id>/bank_withdraw/', views.bwithdraw, name='bwithdraw'),
     path('<int:bank_id>/bank_deposit/', views.bdeposit, name='bdeposit'),
