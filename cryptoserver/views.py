fdeef448f91c4c44d14d8679a0a4e3674ae63163
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-24 01:28:09 -0600

    Modified signin view

--- cryptoserver/views.py
+++ cryptoserver/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
+from .models import Bank, Trader, Company
 
 def index(request):
     return render(request, 'cryptoserver/index.html')
 
-
 def signin(request):
+    username = request.POST['InputName']
     selection = request.POST['inlineFormCustomSelect']
+
     if selection == '1':
-        return render(request, 'cryptoserver/bank_main.html')
+        bank = Bank.objects.filter(username=username)
+        if bank.exists():
+            return render(request, 'cryptoserver/bank_main.html', {'bank': bank})
+        else:
+            return render(request, 'cryptoserver/Error_404.html')
+
     elif selection == '2':
-        return render(request, 'cryptoserver/company_main.html')
+        company = Company.objects.filter(username=username)
+        if company.exists():
+            return render(request, 'cryptoserver/company_main.html',  {'company': company})
+        else:
+            return render(request, 'cryptoserver/Error_404.html')
     else:
-        return render(request, 'cryptoserver/trader_main.html')
+        trader = Trader.objects.filter(username=username)
+        if trader.exists():
+            return render(request, 'cryptoserver/trader_main.html', {'trader': trader})
+        else:
+            return render(request, 'cryptoserver/Error_404.html')
+
