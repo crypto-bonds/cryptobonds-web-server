d4aa63fe0648d343d0f67fc38bf5fec80a51ac2c
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-24 04:31:53 -0600

    Added Bond model into Admin interface

--- cryptoserver/admin.py
+++ cryptoserver/admin.py
@@ -1,6 +1,7 @@
-from .models import Bank, Trader, Company
+from .models import Bank, Trader, Company, Bond
 from django.contrib import admin
 
 admin.site.register(Bank)
 admin.site.register(Trader)
 admin.site.register(Company)
+admin.site.register(Bond)
