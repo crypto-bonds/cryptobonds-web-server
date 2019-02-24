47c51399ce2fdb205d4a4da5aa2672649638d464
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-23 21:14:18 -0600

    Setup models

--- cryptoserver/models.py
+++ cryptoserver/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Bank(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    money = models.IntegerField()
+    public_key = models.CharField(max_length=500)
+
+
+class Trader(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    money = models.IntegerField()
+
+
+class Company(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    money = models.IntegerField()
+
+
+class Bond(models.Model):
+    issuer = models.ForeignKey(Company, on_delete=models.CASCADE)
+    underwriter = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    denomination = models.IntegerField()
+    interest_rate = models.IntegerField()
+    maturity_date = models.DateTimeField()
