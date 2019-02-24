98fd999d5a777eb98ff06fedefa25e04829a4a99
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-23 22:10:01 -0600

    Add signin view

--- cryptoserver/views.py
+++ cryptoserver/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'cryptoserver/index.html')
+
+
+def signin(request):
+    return render(request, 'cryptoserver/bank_main.html')
