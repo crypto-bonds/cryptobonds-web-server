9eb77d362fa46c750d0bb15478ebfd1dfa1cbbb7
Ali Asgher <ali-asgher@uiowa.edu>
2019-02-23 22:09:35 -0600

    Fixed urls

--- crypto_bonds_server/urls.py
+++ crypto_bonds_server/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('cryptoserver/', include('cryptoserver.urls')),
     path('admin/', admin.site.urls),
+    path('cryptoserver/', include('cryptoserver.urls')),
 ]
--- cryptoserver/urls.py
+++ cryptoserver/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signin', views.signin, name='signin'),
 ]
